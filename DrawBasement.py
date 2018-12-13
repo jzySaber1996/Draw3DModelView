5df653a2481fa6d3a055f83b62ebd9d3654e0360
jzySaber1996 <529930262@qq.com>
2018-12-13 21:29:25 +0800

    3D drawing finished

--- DrawBasement.py
+++ DrawBasement.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
-def draw_single_layer():
+def draw_single_layer(height):
     list_x = []
     list_y = []
     count = 0
@@ -22,7 +23,7 @@ def draw_single_layer():
     y_last = list_y[len(list_y) - 1]
     del list_x[len(list_x) - 1]
     del list_y[len(list_y) - 1]
-    plt.plot(list_x, list_y)
+    plt.plot(list_x, list_y, 'red')
 
     list_x = []
     list_y = []
@@ -41,8 +42,88 @@ def draw_single_layer():
             count += 1
     list_x.insert(0, x_last)
     list_y.insert(0, y_last)
-    plt.plot(list_x, list_y)
+    plt.plot(list_x, list_y, 'aqua')
     plt.show()
 
 
 # draw_single_layer()
+def draw_all_layer(height, draw_x_list_perimeter, draw_y_list_perimeter, draw_z_list_perimeter,
+                   draw_x_list_infill, draw_y_list_infill, draw_z_list_infill):
+    list_x = []
+    list_y = []
+    list_z = []
+    count = 0
+    file2 = open("data_perimeter.txt")
+    while 1:
+        line = file2.readline()
+        if not line:
+            break
+        if "X" in line and "Y" in line:
+            res = line.split()
+            x = float(res[1][1:])
+            y = float(res[2][1:])
+            list_x.insert(count, x)
+            list_y.insert(count, y)
+            list_z.insert(count, height)
+            count += 1
+    x_last = list_x[len(list_x) - 1]
+    y_last = list_y[len(list_y) - 1]
+    z_last = list_z[len(list_z) - 1]
+    del list_x[len(list_x) - 1]
+    del list_y[len(list_y) - 1]
+    del list_z[len(list_z) - 1]
+    draw_x_list_perimeter.insert(len(draw_x_list_perimeter), list_x)
+    draw_y_list_perimeter.insert(len(draw_y_list_perimeter), list_y)
+    draw_z_list_perimeter.insert(len(draw_z_list_perimeter), list_z)
+
+    list_x = []
+    list_y = []
+    list_z = []
+    count = 0
+    file = open("data_infill.txt")
+    while 1:
+        line = file.readline()
+        if not line:
+            break
+        if "X" in line and "Y" in line:
+            res = line.split()
+            x = float(res[1][1:])
+            y = float(res[2][1:])
+            z = height
+            # z = float(res[])
+            list_x.insert(count, x)
+            list_y.insert(count, y)
+            list_z.insert(count, z)
+            count += 1
+    list_x.insert(0, x_last)
+    list_y.insert(0, y_last)
+    list_z.insert(0, z_last)
+
+    draw_x_list_infill.insert(len(draw_x_list_infill), list_x)
+    draw_y_list_infill.insert(len(draw_y_list_infill), list_y)
+    draw_z_list_infill.insert(len(draw_z_list_infill), list_z)
+    return draw_x_list_perimeter, draw_y_list_perimeter, draw_z_list_perimeter, \
+        draw_x_list_infill, draw_y_list_infill, draw_z_list_infill
+
+
+def draw_all_layers_main(dict_store):
+    draw_x_list_perimeter = dict_store['draw_x_list_perimeter']
+    draw_y_list_perimeter = dict_store['draw_y_list_perimeter']
+    draw_z_list_perimeter = dict_store['draw_z_list_perimeter']
+    draw_x_list_infill = dict_store['draw_x_list_infill']
+    draw_y_list_infill = dict_store['draw_y_list_infill']
+    draw_z_list_infill = dict_store['draw_z_list_infill']
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.set_title("3D Model")
+    for i in range(len(draw_x_list_perimeter)):
+        listx = draw_x_list_perimeter[i]
+        listy = draw_y_list_perimeter[i]
+        listz = draw_z_list_perimeter[i]
+        ax.plot(listx, listy, listz, 'red')
+    for i in range(len(draw_x_list_infill)):
+        listx = draw_x_list_infill[i]
+        listy = draw_y_list_infill[i]
+        listz = draw_z_list_infill[i]
+        ax.plot(listx, listy, listz, 'aqua')
+    plt.show()
--- DrawLayersResult.py
+++ DrawLayersResult.py
@@ -1,5 +1,7 @@
 import DrawBasement
 from xml.etree.ElementTree import parse
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def print_single_layer(layer_count, perimeter_count):
@@ -51,11 +53,100 @@ def print_single_layer(layer_count, perimeter_count):
                         line = g_code_file.readline()
 
 
+def print_all_layers(perimeter_count):
+    # point = PointPrint(0.5, 0.5)
+    # print(point._PointPrint__a)
+    g_code_file = open("NewOutput.txt")
+    count_total = -1
+    height = 0.0
+    height_next = 0.0
+    count_print_total = 0
+    file_perimeter = open("data_perimeter.txt", "w+")
+    file_infill = open("data_infill.txt", "w+")
+    mark_print = False
+    mark_exit = False
+    mark_return = False
+    draw_x_list_perimeter = []
+    draw_y_list_perimeter = []
+    draw_z_list_perimeter = []
+    draw_x_list_infill = []
+    draw_y_list_infill = []
+    draw_z_list_infill = []
+    while 1:
+        line = g_code_file.readline()
+        if not line:
+            print("finish!")
+            break
+        if "G1 Z" in line:
+            count_total += 1
+            if count_total >= 1:
+                height_string = line.split(" ")[1]
+                height = float(height_string[1: len(height_string)])
+                while 1:
+                    if "G1 X" in line:
+                        file_perimeter.write(line)
+                    if "G1 F" in line:
+                        count_print_total += 1
+                        if count_print_total <= perimeter_count:
+                            while 1:
+                                line = g_code_file.readline()
+                                if "G1 F" in line:
+                                    # count_print_total += 1
+                                    mark_print = True
+                                    break
+                                if "G1 X" in line:
+                                    file_perimeter.write(line)
+                        if count_print_total == perimeter_count + 1:
+                            while 1:
+                                line = g_code_file.readline()
+                                if "M140 S0" in line:
+                                    mark_return = True
+                                    break
+                                if "G1 Z" in line:
+                                    mark_exit = True
+                                    height_string = line.split(" ")[1]
+                                    height_next = float(height_string[1: len(height_string)])
+                                    break
+                                if "G1 X" in line:
+                                    file_infill.write(line)
+                    if mark_exit:
+                        file_perimeter.close()
+                        file_infill.close()
+                        draw_x_list_perimeter, draw_y_list_perimeter, \
+                            draw_z_list_perimeter, draw_x_list_infill, \
+                            draw_y_list_infill, draw_z_list_infill = \
+                            DrawBasement.draw_all_layer(height, draw_x_list_perimeter, draw_y_list_perimeter,
+                                                        draw_z_list_perimeter, draw_x_list_infill,
+                                                        draw_y_list_infill, draw_z_list_infill)
+                        print("finish one layer!")
+                        file_perimeter = open("data_perimeter.txt", "w+")
+                        file_infill = open("data_infill.txt", "w+")
+                        mark_print = False
+                        mark_exit = False
+                        count_print_total = 0
+                        height = height_next
+                        continue
+                    if not mark_print:
+                        line = g_code_file.readline()
+                    if mark_return:
+                        break
+        if mark_return:
+            break
+    dict_store = {'draw_x_list_perimeter': draw_x_list_perimeter,
+                  'draw_y_list_perimeter': draw_y_list_perimeter,
+                  'draw_z_list_perimeter': draw_z_list_perimeter,
+                  'draw_x_list_infill': draw_x_list_infill,
+                  'draw_y_list_infill': draw_y_list_infill,
+                  'draw_z_list_infill': draw_z_list_infill}
+    DrawBasement.draw_all_layers_main(dict_store)
+
+
 if __name__ == '__main__':
     et = parse("printParameters.xml")
     root = et.getroot()
     print(root)
     et_print = root.findall('entry[@key="shellLayers"]')[0]
     data = int(et_print.text)
-    print_single_layer(30, data)
-    DrawBasement.draw_single_layer()
+    print_single_layer(2, data)
+    DrawBasement.draw_single_layer(2)
+    print_all_layers(3)
