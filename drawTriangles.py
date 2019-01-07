5d47861b5021f7f9fefa50a4aaf2b8f5fd04ef24
jzySaber1996 <529930262@qq.com>
2019-01-07 20:43:39 +0800

    painting function

--- drawTriangles.py
+++ drawTriangles.py
@@ -43,6 +43,19 @@ def draw_triangles():
             z_print.insert(0, result_z[i][list_index[j][0]])
             z_print.insert(1, result_z[i][list_index[j][1]])
             ax.plot(x_print, y_print, z_print, 'aqua')
+    file_print = open("singlePointList.txt")
+    x_print_final, y_print_final, z_print_final = [], [], []
+    count_print_final = 0
+    while 1:
+        line = file_print.readline()
+        if not line:
+            break
+        line_list = line.split(" ")
+        x_print_final.insert(count_print_final, float(line_list[0]))
+        y_print_final.insert(count_print_final, float(line_list[1]))
+        z_print_final.insert(count_print_final, float(line_list[2]))
+        count_print_final += 1
+    ax.plot(x_print_final, y_print_final, z_print_final, 'red')
     plt.show()
     return
 
